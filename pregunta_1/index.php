314dd9fb71ad65e2c796a0b1d9902905218bed7c
Leandro Gongora <crisgongor7@gmail.com>
2022-02-04 14:40:03 -0400

    Agregue actualizada

--- pregunta_1/index.php
+++ pregunta_1/index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	 	
-	<title>FACULTAD DE CIENCIAS PURAS Y NATURALES INFORMATICA</title>
+	<title>FACULTAD DE CIENCIAS PURAS Y NATURALES INFORMATICA ACTUALIZADA</title>
 	<meta charset="utf-8"/>		
 	<link rel="stylesheet" href="estilos/estiloinicio.css">				 
 	<link rel="shortcut icon" href="imagenes/logo.png" type="image/x-icon">
